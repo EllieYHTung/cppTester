82f37ba97aefd3fe43107e7843abbb3dd6fdc845
ellie-tung <ellie.tung@foxconn.com>
2019-03-30 23:48:46 +0800

    Initial Commit

---
+++ cppTester/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppTester
+//
+//  Created by YunHua Tung on 2019/3/30.
+//  Copyright © 2019 YunHua Tung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
